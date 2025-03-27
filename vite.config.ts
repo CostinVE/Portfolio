e7acd12f7b49ea7b535ab0e897898f9fbba8492e
CostinVE <costinve@gmail.com>
2025-03-27 21:00:57 +0200

    Beta testing

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './Portfolio', // Use './' for relative paths
+  base: './', // Use './' for relative paths
 })
