74b5cd890c65eb6ad4f3cc44a686122587e34f5d
CostinVE <costinve@gmail.com>
2025-03-27 23:43:03 +0200

    Beta testing

--- src/components/Skillset.tsx
+++ src/components/Skillset.tsx
@@ -1,5 +1,20 @@
 import React, {useState} from 'react';
 
+
+import CSSImage from '../assets/CSS_icon.png';
+import JSImage from '../assets/JS_icon.png';
+import TypeScriptImage from '../assets/typescript_icon.png';
+import ReactImage from '../assets/react.svg';
+import RestApiImage from '../assets/RestApi.gif';
+import GraphQLImage from '../assets/graphql_icon.png';
+import SQLImage from '../assets/SQL_icon.png';
+import MongoDBImage from '../assets/mongodb.png';
+import CRUDImage from '../assets/CRUD_icon.png';
+import NodeJSImage from '../assets/nodejs-logo.svg';
+import GitHubImage from '../assets/GitHUB.svg';
+import MicrosoftOfficeImage from '../assets/MicrosoftOffice.png';
+import FigmaImage from '../assets/Figma.png';
+
 type Skill = {
   id: number;
   label: string;
@@ -12,39 +27,39 @@ const skills: Skill[] = [
   {
     id: 1,
     label: 'CSS/HTML',
-    icon: './src/assets/CSS_icon.png', // Replace with the actual path to your icon
+    icon: CSSImage,
     description: 'CSS and HTML are the building blocks of web development, providing structure and styling for web pages. Leverage semantic HTML for accessibility and CSS features like Flexbox, Grid, and animations to create responsive, visually appealing designs. Mastering these tools ensures your applications are user-friendly and optimized for various devices.'
 
   },
   {
     id: 2,
     label: 'JavaScript',
-    icon: './src/assets/JS_icon.png', // Replace with the actual path to your icon
+    icon: JSImage,
     description: 'JavaScript enables dynamic and interactive web applications by manipulating the DOM and handling events. Use it with modern ES6+ features to build client-side logic and integrate it with libraries like React for scalable SPAs. Its versatility allows you to create everything from simple scripts to complex, feature-rich applications.'
 
   },
   {
     id: 3,
     label: 'TypeScript',
-    icon: './src/assets/typescript_icon.png', // Replace with the actual path to your icon
+    icon: TypeScriptImage,
     description: 'TypeScript adds static typing to JavaScript, improving code maintainability and reducing runtime errors. Use it with React to build robust, scalable applications with better tooling and type safety. It also enhances collaboration in teams by enforcing type contracts and improving code readability.',
   },
   {
     id: 4,
     label: 'ReactJS',
-    icon: './src/assets/react.svg', // Replace with the actual path to your icon
+    icon:  ReactImage,
     description: 'React is a library for building reusable UI components and managing state efficiently. Use it to create dynamic SPAs, leveraging tools like React Router for navigation and React Query for data fetching. Its component-based architecture simplifies development and makes your codebase more modular and maintainable.',
   },
   {
     id: 5,
     label: 'REST API',
-    icon: './src/assets/RestApi.gif', // Replace with the actual path to your icon
+    icon: RestApiImage,
     description: 'REST APIs enable communication between the frontend and backend using HTTP methods. Use them to fetch and send data, integrating with tools like Axios or Fetch API for seamless interaction. REST is widely supported and provides a standardized way to connect your application to external services.',
   },
   {
     id: 6,
     label: 'GraphQL',
-    icon: './src/assets/graphql_icon.png', // Replace with the actual path to your icon
+    icon: GraphQLImage,
     description: 'GraphQL allows clients to request only the data they need, reducing over-fetching. Use it with Apollo Client in React for efficient data querying and mutation in modern applications. Its flexibility makes it ideal for complex applications with diverse data requirements.',
   },
 ];
@@ -53,25 +68,25 @@ const skills2: Skill[] = [
   {
     id: 1,
     label: 'SQL',
-    icon: './src/assets/SQL_icon.png',
+    icon: SQLImage,
     description: 'SQL is used to manage relational databases, enabling efficient data storage and retrieval. Use it with ORMs like Sequelize to simplify database interactions in backend applications. Its structured approach ensures data integrity and is ideal for applications requiring complex relationships between entities.',
   },
   {
     id: 2,
     label: 'MongoDB',
-    icon: './src/assets/mongodb.png',
+    icon: MongoDBImage,
     description: 'MongoDB is a NoSQL database ideal for unstructured or semi-structured data. Use it with Mongoose for schema validation and scalable backend solutions. Its flexibility makes it perfect for real-time applications, such as chat systems or analytics dashboards.',
   },
   {
     id: 3,
     label: 'CRUD Operations',
-    icon: './src/assets/CRUD_icon.png',
+    icon: CRUDImage,
     description: 'CRUD operations (Create, Read, Update, Delete) are essential for managing data in applications. Implement them in backend APIs using frameworks like Express.js or NestJS. They form the backbone of most web applications, enabling seamless interaction with databases.',
   },
   {
     id: 4,
     label: 'NodeJS',
-    icon: './src/assets/nodejs-logo.svg',
+    icon: NodeJSImage,
     description: 'Node.js is a runtime for running JavaScript on the server, ideal for scalable, real-time applications. Use it with Express.js for routing and middleware or NestJS for structured backend development. Its non-blocking architecture makes it highly efficient for handling concurrent requests.',
   },
 ];
@@ -80,19 +95,19 @@ const skills3: Skill[] = [
   {
     id: 1,
     label: 'GitHub',
-    icon: './src/assets/GitHUB.svg',
+    icon: GitHubImage,
     description: 'GitHub is a platform for version control and collaboration. Use it to manage code repositories, track changes, and collaborate with teams using pull requests and branches. It also integrates with CI/CD pipelines, enabling automated testing and deployment workflows.',
   },
   {
     id: 2,
     label: 'Microsoft Office',
-    icon: './src/assets/MicrosoftOffice.png',
+    icon: MicrosoftOfficeImage,
     description: 'Microsoft Office tools like Excel and PowerPoint are essential for data analysis, documentation, and presentations. Use them to organize project data, create reports, and communicate effectively with stakeholders. Excel’s advanced features, like pivot tables and formulas, are particularly useful for tracking project metrics.',
   },
   {
     id: 3,
     label: 'Graphic Design / UI',
-    icon: './src/assets/Figma.png',
+    icon: FigmaImage,
     description: 'Tools like Figma or Adobe XD help create visually appealing designs and prototypes. Use them to design user interfaces and ensure a seamless user experience before development. These tools also facilitate collaboration between developers and designers, ensuring the final product aligns with user needs.',
   },
 ];
